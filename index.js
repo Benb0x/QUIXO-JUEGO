e8cd8fef289889689f5e0c13900cdc467f17ec87
Bárbara <benboxuwu@gmail.com>
2024-09-09 00:13:24 -0600

    kk

--- index.js
+++ index.js
@@ -18,19 +18,20 @@ document.addEventListener('DOMContentLoaded', function() {
             this.botonesBloqueados = true;
             this.botones = Array.from(botonesJuego);
             this.sonidosBoton = [];
-            this.inactividadTimeout = null; // Añadir un temporizador de inactividad
-            this.cargarSonidos();
+            this.inactividadTimeout = null;
+
             this.display = {
                 botonEmpezar,
                 ronda,
                 estadoJuego
             };
+
+            this.cargarSonidos();
             this.iniciar();
         }
 
         iniciar() {
-            this.cargarSonidos();
-
+            // Cargar sonidos y configurar los botones
             this.display.botonEmpezar.addEventListener('click', this.iniciarJuego.bind(this));
             this.botones.forEach(boton => {
                 boton.style.fill = boton.getAttribute('data-color-inactivo');
@@ -42,7 +43,7 @@ document.addEventListener('DOMContentLoaded', function() {
                 });
             });
         }
-        
+
         async cargarSonidos() {
             const sonidos = [
                 'sounds/sounds_1 (1).mp3',
@@ -50,8 +51,9 @@ document.addEventListener('DOMContentLoaded', function() {
                 'sounds/sounds_3 (1).mp3',
                 'sounds/sounds_4 (1).mp3',
                 'sounds/sounds_error (1).wav',
-                'sounds/win.ogg' 
+                'sounds/win.ogg'
             ];
+
             const promesas = sonidos.map((sonido, indice) => {
                 return new Promise((resolve, reject) => {
                     const audio = new Audio(sonido);
@@ -59,59 +61,58 @@ document.addEventListener('DOMContentLoaded', function() {
                         this.sonidosBoton[indice] = audio;
                         resolve();
                     }, { once: true });
-                    audio.addEventListener('error', () => reject(new Error(`Failed to load sound: ${sonido}`)));
+                    audio.addEventListener('error', () => reject(new Error(`Error al cargar el sonido: ${sonido}`)));
                 });
             });
+
             try {
                 await Promise.all(promesas);
+                console.log('Todos los sonidos se han cargado correctamente.');
             } catch (error) {
-                console.error("Error loading sounds:", error);
+                console.error('Error al cargar los sonidos:', error);
             }
         }
-
-        
 
         iniciarJuego() {
             this.display.botonEmpezar.disabled = true;
             this.actualizarRonda(0);
             this.posicionUsuario = 0;
             this.secuencia = this.crearSecuencia();
-            this.resetEstadoJuego();  
+            this.resetEstadoJuego();
             this.mostrarSecuencia();
         }
 
         resetEstadoJuego() {
             this.display.estadoJuego.textContent = 'Listo para comenzar!';
-            this.display.estadoJuego.style.color = '#4682B4';  // Color predeterminado
-            this.display.estadoJuego.classList.remove('ganador'); // Quita la clase ganador
-            this.display.estadoJuego.style.marginLeft = '20px'; // Resetea el margen izquierdo
+            this.display.estadoJuego.style.color = '#4682B4';
+            this.display.estadoJuego.classList.remove('ganador');
+            this.display.estadoJuego.style.marginLeft = '20px';
         }
 
         actualizarRonda(valor) {
             this.rondaActual = valor;
-            //this.display.ronda.textContent = `Ronda ${this.rondaActual}`;
         }
 
         crearSecuencia() {
-            return Array.from({length: this.rondasTotales}, () => Math.floor(Math.random() * this.botones.length));
+            return Array.from({ length: this.rondasTotales }, () => Math.floor(Math.random() * this.botones.length));
         }
 
         validarColorElegido(indice) {
             if (this.secuencia[this.posicionUsuario] === indice) {
-                clearTimeout(this.inactividadTimeout); // Limpiar el temporizador de inactividad
+                clearTimeout(this.inactividadTimeout);
                 this.alternarEstiloBoton(this.botones[indice], true);
-        
-                // Reproducir el sonido con manejo de errores
+
+                // Reproducir el sonido
                 if (this.sonidosBoton[indice]) {
                     this.sonidosBoton[indice].play().catch(error => {
                         console.error('Error al reproducir el audio:', error);
                     });
                 }
-        
+
                 setTimeout(() => {
                     this.alternarEstiloBoton(this.botones[indice], false);
                 }, this.velocidad / 2);
-        
+
                 if (this.rondaActual === this.posicionUsuario) {
                     this.posicionUsuario = 0;
                     this.rondaActual++;
@@ -124,7 +125,7 @@ document.addEventListener('DOMContentLoaded', function() {
                 } else {
                     this.posicionUsuario++;
                     this.display.estadoJuego.textContent = `Correcto! Sigue así.`;
-                    this.inactividadTimeout = setTimeout(() => this.perderJuego(), 15000); // Establecer el temporizador de inactividad
+                    this.inactividadTimeout = setTimeout(() => this.perderJuego(), 15000);
                 }
             } else {
                 setTimeout(() => this.perderJuego(), 250);
@@ -147,7 +148,7 @@ document.addEventListener('DOMContentLoaded', function() {
                 } else {
                     clearInterval(intervalo);
                     this.botonesBloqueados = false;
-                    this.inactividadTimeout = setTimeout(() => this.perderJuego(), 15000); // Establecer el temporizador de inactividad después de mostrar la secuencia
+                    this.inactividadTimeout = setTimeout(() => this.perderJuego(), 15000);
                 }
             }, this.velocidad);
         }
@@ -163,73 +164,36 @@ document.addEventListener('DOMContentLoaded', function() {
         }
 
         perderJuego() {
-            clearTimeout(this.inactividadTimeout); // Limpiar el temporizador de inactividad
+            clearTimeout(this.inactividadTimeout);
             this.display.estadoJuego.textContent = 'Perdiste. Intenta de nuevo.';
             this.display.estadoJuego.style.color = 'red';
             this.display.botonEmpezar.disabled = false;
             this.botonesBloqueados = true;
             this.resetJuego();
-            
-            // Reproducir el sonido de error
-            if (this.sonidosBoton[4]) {  // Asumiendo que el sonido de error es el índice 4
-                this.sonidosBoton[4].play();
-            }
-        }
 
-        perderJuego() {
-            clearTimeout(this.inactividadTimeout); // Limpiar el temporizador de inactividad
-            this.display.estadoJuego.textContent = 'Perdiste. Intenta de nuevo.';
-            this.display.estadoJuego.style.color = 'red';
-            this.display.botonEmpezar.disabled = false;
-            this.botonesBloqueados = true;
-            this.resetJuego();
-            
-            // Reproducir el sonido de error
-            if (this.sonidosBoton[4]) {  // Asumiendo que el sonido de error es el índice 4
+            if (this.sonidosBoton[4]) {
                 this.sonidosBoton[4].play();
             }
         }
 
         ganarJuego() {
-            clearTimeout(this.inactividadTimeout); // Limpiar el temporizador de inactividad
+            clearTimeout(this.inactividadTimeout);
             this.display.estadoJuego.innerHTML = '¡F E L I C I D A D E S &nbsp;&nbsp;&nbsp; G A N A S T E!';
             this.display.estadoJuego.style.color = 'green';
             this.display.estadoJuego.classList.add('ganador');
-            this.display.estadoJuego.style.marginLeft = '195px'; // Ajusta el valor según lo necesites
+            this.display.estadoJuego.style.marginLeft = '195px';
             this.display.ronda.style.display = 'none';
             this.botonesBloqueados = true;
-        
-            // Reproduce el sonido de victoria
-            if (this.sonidosBoton[5]) {  // Asumiendo que el sonido de victoria es el índice 5
+
+            if (this.sonidosBoton[5]) {
                 this.sonidosBoton[5].play();
             }
-        
-            // Cambia el color de los elementos especificados
-            const nuevoColor = '#5CE261'; // Dorado, ajusta según sea necesario
-            const elementos = [cascoS, cascoI, ojos, bigote];
-        
-            elementos.forEach((elemento, index) => {
-                setTimeout(() => {
-                    elemento.style.fill = nuevoColor;
-                    elemento.classList.add('animacion-ganador');
-                }, index * 100); // Ajusta el tiempo entre cada animación
-            });
-        
-            setTimeout(() => {
-                elementos.reverse().forEach((elemento, index) => {
-                    setTimeout(() => {
-                        elemento.style.fill = '';
-                        elemento.classList.add('animacion-ganador');
-                    }, index * 100); // Ajusta el tiempo entre cada animación
-                });
-            }, elementos.length * 100); // Espera a que termine la primera secuencia
-        
+
             setTimeout(() => {
                 this.display.botonEmpezar.disabled = false;
                 this.resetJuego();
-            }, (elementos.length * 2 * 100) + 1000); // Espera a que termine la animación para habilitar el botón
+            }, 1000);
         }
-        
 
         resetJuego() {
             this.botones.forEach(boton => this.alternarEstiloBoton(boton, false));
